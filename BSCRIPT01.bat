7b0992143ef0cba3ef7251db31f61da0efc00d96
cinnaumiroe <jynyl.yvryl@gmail.com>
2024-07-12 21:19:29 +0800

    Open 5 websites, calculator,notepad, and a system shutdown

---
+++ BSCRIPT01.bat
@@ -0,0 +1,13 @@
+@echo off
+rem open five websites
+start chrome "https://uc-bcf.instructure.com/"
+start chrome "https://www.canva.com/"
+start chrome "https://coolors.co/"
+start chrome "https://archive.com/"
+start chrome "https://www.youtube.com/"
+rem opens calculator and notepad
+start Calc
+start Notepad
+rem initiate system shutdown
+/s /f /t 0
+pause
